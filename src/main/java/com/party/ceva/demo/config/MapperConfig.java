5dceecfe2dc3c19539d9a3ba940f2ffc7e0dc653
Andrei1694 <andrei4.stanciu@gmail.com>
2026-02-27 05:23:54 +0200

    Add ModelMapper dependency for object mapping

---
+++ src/main/java/com/party/ceva/demo/config/MapperConfig.java
@@ -0,0 +1,16 @@
+package com.party.ceva.demo.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        // You can add custom configuration here if needed
+        return modelMapper;
+    }
+}
